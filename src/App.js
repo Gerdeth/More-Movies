1de6f8e757eb269dbe3f2def302856974927f31f
Gerdeth <gerdethwickham@gmail.com>
2021-02-21 07:54:18 -0500

    update image api

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React,{useEffect, useState} from 'react';
 
 import Movie from "./components/Movie"
 
-const FEATURED_API= "https://api.themoviedb.org/3/movie/550?api_key=a607d8d4f02314295a958f2286e64a46";
+const FEATURED_API= "https://api.themoviedb.org/3/discover/movie?sort_by-popularity.desc&api_key=a607d8d4f02314295a958f2286e64a46";
+const IMG_API="https://image.tmdb.org/t/p/w1280";
+const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=a607d8d4f02314295a958f2286e64a46&query";
+
 function App() {
   const [movies,setMovies]= useState([]);
 
   useEffect(()=>{
-    fetch(FEATURED_API).then(res=> res.json()).then(data=>{
-      console.log(data.original_title);
-      setMovies(data);
+    fetch(FEATURED_API).then((res)=> res.json()).then((data)=>{
+      console.log(data);
+      setMovies(data.results);
     });
-    
+  
     
   },[]);
   return (
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,13 @@
 
-import React from 'react'
+import React from 'react';
 
-
-const Movie = ({original_title, overview,poster_path})=>(
+const IMG_API="https://image.tmdb.org/t/p/w1280";
+const Movie = ({title, overview, poster_path, vote_average}) =>(
     <div className= "movie">
+        <img src={IMG_API+ poster_path} alt={title}/>
        <div className="movie-info">
-            <h3> {original_title}</h3>
-            <span>{overview}</span>
+            <h3> {title}</h3>
+            <span>{vote_average}</span>
        </div>
 
 
