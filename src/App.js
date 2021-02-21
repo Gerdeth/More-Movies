1fab34029f3259a44df9b1cb999b6673a807407c
Gerdeth <gerdethwickham@gmail.com>
2021-02-21 10:11:56 -0500

    add search feature

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Movie from "./components/Movie"
 
 const FEATURED_API= "https://api.themoviedb.org/3/discover/movie?sort_by-popularity.desc&api_key=a607d8d4f02314295a958f2286e64a46";
 const IMG_API="https://image.tmdb.org/t/p/w1280";
-const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=a607d8d4f02314295a958f2286e64a46&query";
+const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=a607d8d4f02314295a958f2286e64a46&query=";
 
 function App() {
   const [movies,setMovies]= useState([]);
+  const [searchTerm, setsearchTerm]=useState(" ")
 
   useEffect(()=>{
     fetch(FEATURED_API).then((res)=> res.json()).then((data)=>{
@@ -17,12 +18,43 @@ function App() {
   
     
   },[]);
+
+  const handleOnSubmit=(e)=>{
+    e.preventDefault();
+    fetch(SEARCH_API + searchTerm).then((res)=> res.json()).then((data)=>{
+      setMovies(data.results);
+    });
+
+   
+    
+  };
+
+
+  const handleOnChange=(e)=>{
+    setsearchTerm(e.target.value);
+  };
+
   return (
-    <div className= "movie-container">{
+    <>
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input 
+        type="search" 
+        className="search" 
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleOnChange}
+        />
+      </form>
+    </header>
+    <div className= "movie-container">
+      {
+
       movies.length > 0 && movies.map((movie)=>
         <Movie key={movie.id} {...movie}/>
       )}
     </div>
+    </>
     );
 
   
